f22b3e809082607979a4b69354aeec7d8c8b9474
Hussein Taiseer <hussein.twahba@gmail.com>
2024-11-04 22:40:22 +0200

    ok

---
+++ Business Problem 1 (Increasing_Pet_Adoptions)/2_adoption_related_questions/2.4aggresiveness_related/2.4.1all_pets_aggresive_percentages.sql
@@ -0,0 +1,64 @@
+WITH totals AS (
+    SELECT
+        s.species_name,
+        COUNT(*) AS total_of_species
+    FROM
+        pets p
+        LEFT JOIN species s ON p.species_id = s.species_id
+    GROUP BY
+        s.species_name
+)
+SELECT
+    s.species_name,
+    -- Counts for each aggressiveness level
+    COUNT(
+        CASE
+            WHEN aggressiveness = 'Low' THEN 1
+        END
+    ) AS count_of_low,
+    COUNT(
+        CASE
+            WHEN aggressiveness = 'Medium' THEN 1
+        END
+    ) AS count_of_medium,
+    COUNT(
+        CASE
+            WHEN aggressiveness = 'High' THEN 1
+        END
+    ) AS count_of_high,
+    -- Percentages for each aggressiveness level
+    ROUND(
+        COUNT(
+            CASE
+                WHEN aggressiveness = 'Low' THEN 1
+            END
+        ) * 100.0 / t.total_of_species,
+        2
+    ) AS percent_of_low,
+    ROUND(
+        COUNT(
+            CASE
+                WHEN aggressiveness = 'Medium' THEN 1
+            END
+        ) * 100.0 / t.total_of_species,
+        2
+    ) AS percent_of_medium,
+    ROUND(
+        COUNT(
+            CASE
+                WHEN aggressiveness = 'High' THEN 1
+            END
+        ) * 100.0 / t.total_of_species,
+        2
+    ) AS percent_of_high,
+    -- Total count for reference
+    t.total_of_species
+FROM
+    pets p
+    LEFT JOIN species s ON p.species_id = s.species_id
+    LEFT JOIN totals t ON s.species_name = t.species_name
+GROUP BY
+    s.species_name,
+    t.total_of_species
+ORDER BY
+    count_of_medium DESC;
